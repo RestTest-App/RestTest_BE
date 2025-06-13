f130af51e33e85436a1dc2a8ef045ba6b53abf6f
seoiiwon <wldnjstj99@gmail.com>
2025-06-13 19:02:53 +0900

    ✨ feat[#16]: s3 관련 임시 commit

--- domain/user/repository/user_repository.py
+++ domain/user/repository/user_repository.py
@@ -1,11 +1,21 @@
+from typing import Optional
+
+from sqlalchemy import update, select
+from sqlalchemy.ext.asyncio import AsyncSession
 from domain.user.entity import User
 
 
 class UserRepository:
 
     @staticmethod
-    async def save_user_info(db: AsyncSession, user: User) -> User:
-        db.add(user)
-        await db.commit()
-        await db.refresh(user)
-        return user
+    async def update_user(db: AsyncSession, user_id: int, nickname: str, profile_image: Optional[str]) -> User:
+            await db.execute(
+                update(User)
+                .where(User.id == user_id)
+                .values(nickname=nickname, profile_image=profile_image)
+            )
+            await db.commit()
+            await db.refresh(User)
+
+            result = await db.execute(select(User).where(User.id == user_id))
+            return result.scalar_one()
