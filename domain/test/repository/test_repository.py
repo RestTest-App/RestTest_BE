7149a957ace764ce781472ad9a9ef7db22191f96
seoiiwon <wldnjstj99@gmail.com>
2025-06-16 12:41:21 +0900

    ✨ feat[#30]: repository 작성

--- domain/test/repository/test_repository.py
+++ domain/test/repository/test_repository.py
@@ -1,10 +1,14 @@
-# DB 접근을 위한 로직을 작성 (CRUD) DB 접근에 중점
-from sqlalchemy.orm import Session
+from datetime import datetime
+from typing import Sequence, Dict, List
+from sqlalchemy import select
+from sqlalchemy.dialects.mysql import insert
+from sqlalchemy.ext.asyncio import AsyncSession
+from domain.test.entity import Question, TestTracker, UserQuestionTracker, ExamSection
 from domain.test.entity.exam import Exam
 
 
 class TestRepository:
-    def __init__(self, db: Session):
+    def __init__(self, db: AsyncSession):
         self.db = db
 
     def create_test(self, name: str) -> Exam:
@@ -16,3 +20,55 @@ class TestRepository:
 
     def get_test_all(self) -> list[Exam]:
         return self.db.query(Exam).all()
+
+    # 정답 불러오기 (시험모드)
+    async def get_test_correct_answer(self, exam_id: int) -> Sequence[Question]:
+        result = await self.db.execute(
+            select(Question).where(Question.exam_id == exam_id).order_by(Question.id)
+        )
+        return result.scalars().all()
+
+
+    # test tracker 등록
+    async def create_user_test_tracker(self, user_id: int, exam_id: int, is_passed: bool, correct_count: int, total_count: int, solved_at: datetime) -> TestTracker:
+        test_score = int((correct_count / total_count) * 100)
+        result = await self.db.execute(
+            insert(TestTracker).values(
+                user_id=user_id,
+                exam_id=exam_id,
+                is_passed=is_passed,
+                score=test_score,
+                solved_at=solved_at
+            )
+        )
+        tracker_id = result.inserted_primary_key[0]
+        await self.db.commit()
+        await self.db.refresh(tracker_id)
+        return TestTracker(
+            id=tracker_id,
+            exam_id=exam_id,
+            user_id=user_id,
+            is_passed=is_passed,
+            score=test_score,
+            solved_at=solved_at
+        )
+
+
+    # question trakcer 등록하기
+    async def create_user_question_tracker(
+            self,
+            test_tracker_id: int,
+            records: List[Dict],
+    ) -> None:
+        await self.db.execute(insert(UserQuestionTracker), records)
+        await self.db.commit()
+        await self.db.refresh(test_tracker_id)
+
+
+    # section 불러오기
+    async def get_section_names(self, section_ids: List[int]) -> Dict[int, str]:
+        result = await self.db.execute(
+            select(ExamSection).where(ExamSection.id.in_(section_ids))
+        )
+        sections = result.scalars().all()
+        return {section.id: section.name for section in sections}
